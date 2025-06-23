a141f208a90948d0be6a8f0a49692568e0ccc64b
TGBUG <3770955706@qq.com>
2025-06-24 00:02:39 +0800

    大脑抽了

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import yaml
 import os
 
-CONFIG_PATH = os.getenv("conf.yaml")
+CONFIG_PATH = "conf.yaml"
 
 if not os.path.exists(CONFIG_PATH):
     raise FileNotFoundError(f"配置文件未找到: {CONFIG_PATH}")
